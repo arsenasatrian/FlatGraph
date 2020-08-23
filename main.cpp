3bc5fa3a464f57d648700ced539ab2013b49acd6
arsenasatrian <65194096+arsenasatrian@users.noreply.github.com>
2020-08-23 22:55:21 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include"FlatGraph.h"
 
 
-int main()
+int main(int argc,char **filePath)
 {
-	std::string fileName = "C:/Users/Arsen/desktop/f1.txt";
-	std::unordered_map<std::string, std::shared_ptr<vertex>> umap;
-	std::unordered_map<std::string, std::shared_ptr<path>> shortPath;
-
-	readFromFile(umap, fileName);
+	/*std::string filePath;
+	std::cin >> filePath;*/
+	std::unordered_map<int, std::shared_ptr<vertex>>            umap;
+	std::unordered_map<int, std::vector<std::shared_ptr<path>>> shortPath;
+	readFromFile(umap, filePath[1]);
 	findShortestPath(umap, shortPath);
-	print(shortPath);
-	                   
+	print(shortPath);            
 	return 0;
 }
